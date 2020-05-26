c058b9c56be62aeb3530582bf7138e0bfc94d6d1
Nick <49137044+Nicholas-Castle@users.noreply.github.com>
2020-05-25 19:34:04 -0700

    first commit

---
+++ src/pages/page3.js
@@ -0,0 +1,15 @@
+// Gatsby supports TypeScript natively!
+import React from "react"
+import { PageProps, Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ThirdPage = () => (
+  <Layout>
+    <SEO title="Page two" />
+    <h1>Hi from the second page</h1>
+  </Layout>
+)
+
+export default ThirdPage
